bf563e8bbf89ec2230d476f4be086c6fa4adb00b
Daniel Sarkesian <danielsarkesian@gmail.com>
2022-10-19 14:12:09 -0400

    finished javascript gameboard

--- connect4.js
+++ connect4.js
@@ -7,11 +7,11 @@
  * board fills (tie)
  */
 
-var WIDTH = 7;
-var HEIGHT = 6;
+const WIDTH = 7;
+const HEIGHT = 6;
 
-var currPlayer = 1; // active player: 1 or 2
-var board = []; // array of rows, each row is array of cells  (board[y][x])
+const currPlayer = 1; // active player: 1 or 2
+const board = []; // array of rows, each row is array of cells  (board[y][x])
 
 /** makeBoard: create in-JS board structure:
  *    board = array of rows, each row is array of cells  (board[y][x])
@@ -19,12 +19,21 @@ var board = []; // array of rows, each row is array of cells  (board[y][x])
 
 function makeBoard() {
   // TODO: set "board" to empty HEIGHT x WIDTH matrix array
+  let gameBoard = [];
+  for(let y = 0; y < HEIGHT; y++){
+    let row = [];
+    for(let x=0; x<WIDTH;x++){;
+      row.push(null);
+    }
+    gameBoard.push(row);
+  }
 }
 
 /** makeHtmlBoard: make HTML table and row of column tops. */
 
 function makeHtmlBoard() {
   // TODO: get "htmlBoard" variable from the item in HTML w/ID of "board"
+
 
   // TODO: add comment for this code
   var top = document.createElement("tr");
